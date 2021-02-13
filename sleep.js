ea8d14eba6bcc64dda63ddab0b04f340df5813f5
ilsubyeega <ilsubyeega@gmail.com>
2021-02-13 20:02:17 +0900

    cleanup

--- sleep.js
+++ sleep.js
@@ -1,7 +1,7 @@
 // https://github.com/kr512/sleep.js
 // Reference: https://my-nutri.co.kr/%EC%88%98%EB%A9%B4-%EC%A3%BC%EA%B8%B0-%EA%B3%84%EC%82%B0/
 
-const SleepJS = function() {
+const SleepJS = function () {
 
     /**
      * 추천하는 수면 시간을 반환합니다.
@@ -39,16 +39,17 @@ const SleepJS = function() {
     this.getSleepTime = function (from, cycle) {
         return (from + 0.25 + (1.5 * cycle)) % 24;
     }
+
     /**
      * getSleepTime을 array로 받아옵니다.
      * @param {int} from 잠드는 시간 (현재) 
      * @param {array} cycle 수면 사이클 수의 Array (기본값=6,7)
      * @returns 결과시간 반환 (24시간 이상이면 24시간을 뺀 시간으로 반환함)
      */
-    this.getSleepTimes = function (from, cycles=-1){
-        if (typeof cycles !== "object" || cycles==-1) cycles = [6, 7];
+    this.getSleepTimes = function (from, cycles = -1) {
+        if (typeof cycles !== "object" || cycles == -1) cycles = [6, 7];
         var a = new Array();
-        cycles.forEach(v=> a.push(this.getSleepTime(from, v)));
+        cycles.forEach(v => a.push(this.getSleepTime(from, v)));
         return a;
     }
 
